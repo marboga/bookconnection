3775fedb49ba64c93c350edb5d71324683f27047
Asher Israel <artificialisrael@yahoo.com>
2016-03-31 15:27:03 -0700

    test gitignore

--- apps/login/views.py
+++ apps/login/views.py
@@ -4,20 +4,22 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-    context = RequestContext(request, {'user': request.user})
-    return render_to_response('login/index.html', context_instance=context)
+	context = RequestContext(request, {'user': request.user})
+	return render_to_response('login/index.html', context_instance=context)
 
 def login(request):
-    return render(request, 'login/dashboard.html')
+	return render(request, 'login/dashboard.html')
 
 def logout_view(request):
-    logout(request)
-    return redirect('/')
+	print "logout"
+	logout(request)
+	print "logout redirect"
+	return redirect(home)
 
 def home(request):
-    context = RequestContext(request, {'request': request, 'user': request.user})
-    return render_to_response('login/home.html', context_instance=context)
+	context = RequestContext(request, {'request': request, 'user': request.user})
+	return render_to_response('login/home.html', context_instance=context)
 
-@login_required(login_url='/')
+@login_required(login_url='/home')
 def dashboard(request):
-    return render_to_response('login/dashboard.html')
+	return render_to_response('login/dashboard.html')
