f7640e41d03a94601993bc1892545c198d827ea8
Asher Israel <artificialisrael@yahoo.com>
2016-04-01 10:17:26 -0700

    show user tweets in different color

--- apps/login/views.py
+++ apps/login/views.py
@@ -53,8 +53,9 @@ def dashboard(request):
 
 	# auth = tweepy.OAuthHandler(consumer_key, consumer_token)
 	auth.set_access_token(access_token, access_token_secret)
-	api = tweepy.API(auth)
+	api = tweepy.API(auth, wait_on_rate_limit_notify=True)
 	# api.update_status('tweepy + oauth!')
-	tweets = api.search(geocode="47.60994,-122.19666,4mi")
-	context = RequestContext(request, {'request': request, 'user':request.user, 'tweets': tweets})
+	tweets = api.search(count="100",geocode="47.60994,-122.19666,1.5mi")
+	user_tweets = api.user_timeline(count="50")
+	context = RequestContext(request, {'request': request, 'user':request.user, 'tweets': tweets, 'user_tweets': user_tweets})
 	return render_to_response('login/dashboard.html', context_instance=context)
